d1b2d70698ffcd645556a87262f572f47c14ab72
Ivy Mead <16579295+imead@users.noreply.github.com>
2022-04-07 18:59:26 -0400

    More data processing

--- ElectricVehicles2020Analysis.jl
+++ ElectricVehicles2020Analysis.jl
@@ -1,31 +1,76 @@
-## Download required packages
+###################################### Electric Vechicles 2020 Analysis ###################################################
+### PROGRAM: ElectricVehicles2020Analysis.jl
+### PURPOSE: 
+### PROGRAMMER: Ivy Mead
+### DATE AUTHORED: 04/06/22 (updated: 04/07/22)
+### LANGUAGES: Julia
+### PACKAGES/DEPENDENCIES: CSV.jl, DataFrames.jl
+### DATA SOURCES:
+### END REPORTING:
+##########################################################################################################################
+
+## Download required Julia packages
 using Pkg
 Pkg.add("CSV")
 Pkg.add("DataFrames")
 
-## Load required packages
+## Load required Julia packages
 using CSV
 using DataFrames
 
-## Read in first CSV
-a1 = CSV.File("Colorado_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv")
+## Find active directory to check path, note double backslashes for this application (vs UNIX)
+pwd()
 
-Colorado = CSV.read("Colorado_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; header=6, DataFrame)
-
-## pass to DataFrame
 ## 04/06/22 running into issues with the header and also with IOBuffer (from CSV.jl documentation)
 californiadf = CSV.File("California_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; header=2)
 
-a = CSV.read("test.csv", DataFrame)
+## Load in all CSVs into a single DataFrame, trying approach using broadcasting because it should allow me to use the
+## header function in CSV.js to bypass the description text at the beginning of the file.
+## This works to load in three CSVs, recoding headers and skipping beginning section; however, I need to create a
+## new column to identify state.
+RegistrationData = DataFrame.(CSV.File.(["California_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv", 
+    "Colorado_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv", 
+    "Florida_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"
+    ]; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
 
-## Find active directory to check path 
-pwd()
 
-## This give valid sink argument error, this code is used in all the examples so it should work
-## Also checked with full path and it still gave an error. Seems like it is an issue with VS Code.
-CSV.read("C:\\Users\\ivylm\\Git\\ElectricVehicles\\test.csv", DataFrame)
+## Load in single CSV for California and then add new column with state
+californiadf = DataFrame.(CSV.File.("California_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
 
-CSV.read("C:\\Users\\ivylm\\Git\\ElectricVehicles\\California_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv", DataFrame)
+## Got an error because I was trying to assign State as a literal which is incorrect, no single or double quotes to name.
+insertcols!(californiadf, 3, :State => "California")
 
-## Trying to get CSV.jl function skipto to work to remove text header
-Colorado = CSV.read("C:\\Users\\ivylm\\Git\\ElectricVehicles\\Colorado_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv", DataFrame, header=6)
+## Do the same for another CSV
+coloradodf = DataFrame.(CSV.File.("Colorado_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+insertcols!(coloradodf, 3, :State => "Colorado")
+
+## Test appending the two CSV together
+GasPricesUS = vcat(californiadf, coloradodf)
+
+## Prep all the remaining state datasets for gas prices
+floridadf = DataFrame.(CSV.File.("Florida_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+massachusettsdf = DataFrame.(CSV.File.("Massachusetts_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+minnesotadf = DataFrame.(CSV.File.("Minnesota_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+newyorkdf = DataFrame.(CSV.File.("New_York_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+ohiodf = DataFrame.(CSV.File.("Ohio_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+texasdf = DataFrame.(CSV.File.("Texas_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+washingtondf = DataFrame.(CSV.File.("Washington_All_Grades_All_Formulations_Retail_Gasoline_Prices.csv"; skipto=6, header=["Year", "Gasoline Prices(Dollars)"]))
+
+## Not sure how to loop in this instance so continuing to do multiple line
+insertcols!(floridadf, 3, :State => "Florida")
+insertcols!(massachusettsdf, 3, :State => "Massachusetts")
+insertcols!(minnesotadf, 3, :State => "Minnesota")
+insertcols!(newyorkdf, 3, :State => "New York")
+insertcols!(ohiodf, 3, :State => "Ohio")
+insertcols!(texasdf, 3, :State => "Texas")
+insertcols!(washingtondf, 3, :State => "Washington")
+
+## Clear variable
+GasPricesUS = 0
+
+## Append all prepped 
+GasPricesUS = vcat(californiadf, coloradodf, floridadf, massachusettsdf, minnesotadf,
+    newyorkdf, ohiodf, texasdf, washingtondf)
+
+## Add PADD Designation
+df.newcol = ifelse.((df.A .== 2) .& (df.B .== "F"), "bingo", "no bingo"); df
